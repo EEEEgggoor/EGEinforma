3d5e2baa0b6cc2bbc155595b2beff6146c48cab6
telebot <ivanstepanov450>
2023-04-20 09:18:17 +0300

    /

--- classwork/classinfprog.py
+++ classwork/classinfprog.py
@@ -26,3 +26,31 @@
 #         if (l[i] % 160 != l[j] % 160) and (l[i] % 7 == 0 or l[j] % 7 == 0):
 #             ans.append(l[i] + l[j])
 # print(len(ans), max(ans))
+
+
+# # Решу егэ №17 - 48465
+# l = [int(i) for i in open('17_3.txt')]
+# ans = []
+# # поиск минимального числа, оканчивающегося на 6
+# min6 = 10001
+# for i in range(len(l)):
+#     if str(l[i])[-1] == '6':
+#         min6 = min(min6, l[i])
+# # пары
+# for i in range(len(l)-1):
+#     if ((str(l[i])[-1]=='6' and str(l[i+1])[-1] != '6') or (str(l[i])[-1]!='6' and str(l[i+1])[-1] == '6')) and ((l[i]**2 + l[i+1]**2)< min6**2):
+#         ans.append((l[i]**2 + l[i+1]**2))
+# print(len(ans), max(ans))
+
+
+#Реуш егэ №17 - 47221
+l = [int(i) for i in open('17_4.txt')]
+ans = []
+max3 = -10001
+for i in range(len(l)):
+    if str(l[i])[-1] == '3':
+        max3 = max(max3, l[i])
+for i in range(len(l)-1):
+    if ((str(l[i])[-1]=='3' and str(l[i+1])[-1] != '3') or (str(l[i])[-1]!='3' and str(l[i+1])[-1] == '3')) and ((l[i]**2 + l[i+1]**2) >= max3**2):
+        ans.append((l[i]**2 + l[i+1]**2))
+print(len(ans), max(ans))
