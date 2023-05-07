1d08a26029f9535999ce8eed255663479285c0b8
Георгий Лыгач <glugach3@gmail.com>
2023-05-08 01:23:47 +0300

    8.05.2023

--- dz-7.05.23/решу-егэ(11545155).py
+++ dz-7.05.23/решу-егэ(11545155).py
@@ -98,4 +98,73 @@
 # print('21)', [s for s in range(1, 65) if f(s, 4)])
 
 
-#номер-23
+# номер-23
+# def F(s, f):
+#     if s>f or s==12: return 0
+#     if s==f: return 1
+#     if s<f: return F(s+1, f) + F(s+3, f)
+#
+# print(F(1, 10) + F(10, 15))
+
+
+# номер-24
+# k=1
+# n=0
+# l = open("24.txt").readline()
+# for i in range(1, len(l)):
+#     if (l[i]=='K' and l[i-1]=='L') or (l[i]=='L' and l[i-1]=='K'):
+#         k=1
+#     else:
+#         k+=1
+#         if k>n:
+#             n=k
+# print(n)
+
+
+# номер 25
+# def div(n):
+#     s = set()
+#     for i in range(2, int(n ** 0.5) + 1):
+#         if n % i == 0:
+#             s.add(i)
+#             s.add(n // i)
+#     return sorted(s)
+#
+#
+# def m(n):
+#     if len(n) >= 5:
+#         return sorted(n)[0] * sorted(n)[1] * sorted(n)[2] * sorted(n)[3] * sorted(n)[4]
+#     if len(n) < 5:
+#         return 0
+#
+#
+# k = 0
+# for n in range(200_000_000, 1 * 10 ** 9):
+#     if len(div(n)) >= 5:
+#         if 0 < m(div(n)) < n:
+#             k += 1
+#             print(m(div(n)))
+#             if k == 6:
+#                 break
+
+
+#номер-26
+summ=0
+k=0
+sp=[]
+with open('26.txt') as f:
+    s = [x for x in f]
+    st = int(s[0][:5])
+    s = [int(x) for x in s[1:]]
+    s = sorted(s, reverse=False)
+    for i in range(1, len(s)):
+        summ +=s[i]
+        if summ<st:
+            k+=1
+            sp.append(s[i])
+
+print(len(sp), sum(sp), max(sp))
+
+
+
+
