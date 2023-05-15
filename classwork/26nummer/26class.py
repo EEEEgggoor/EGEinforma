27f1b137c7c04108ffb5493cf6fa7d8f1338c879
telebot <ivanstepanov450>
2023-05-15 13:44:45 +0300

    26

---
+++ classwork/26nummer/26class.py
@@ -0,0 +1,11 @@
+# КЕГЭ - 6056
+# s = [int(x) for x in open('26.txt')]
+# s = sorted(s[1:], reverse=True)
+# k, mini = 1, s[0]
+# for i in range(1, len(s)):
+#     if s[i] + 56 <= mini:
+#         mini = s[i]
+#         k += 1
+# print(k, mini)
+
+
