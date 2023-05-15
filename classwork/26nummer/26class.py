5362690a43af5d24ee77d7287312f39e20fb2ce6
telebot <ivanstepanov450>
2023-05-15 14:26:24 +0300

    26

--- classwork/26nummer/26class.py
+++ classwork/26nummer/26class.py
@@ -1,6 +1,6 @@
 # КЕГЭ - 6056
-# s = [int(x) for x in open('26.txt')]
-# s = sorted(s[1:], reverse=True)
+# s = [int(x) for x in open('26.txt')][1:]
+# s = sorted(s, reverse=True)
 # k, mini = 1, s[0]
 # for i in range(1, len(s)):
 #     if s[i] + 56 <= mini:
@@ -9,3 +9,23 @@
 # print(k, mini)
 
 
+# КЕГЭ - 5228
+# s = [int(x) for x in open('26_1.txt')][1:]
+# s = sorted(s, reverse=True)
+# k, mx = 1, s[0]  # начинаем с первого передачика, потому k=1
+# for i in range(0, len(s)):
+#     if s[i] + 8 <= mx:
+#         mx = s[i]
+#         k += 1
+# print(k, mx)
+
+
+#  поляков - 5325
+# s = [int(x) for x in open('26-2.txt')][1:]
+# s = sorted(s, reverse=True)
+# k, mx = 1, s[0]
+# for i in range(0, len(s)):
+#     if s[i]+3<=mx:
+#         mx = s[i]
+#         k+=1
+# print(k ,mx)
