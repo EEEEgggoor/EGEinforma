37c1be937d6af481cfd21f70ff469384edc4f9c3
Student <glugach3@gmail.com>
2023-09-05 10:17:30 +0300

    1

--- demo-1.09/1.09.py
+++ demo-1.09/1.09.py
@@ -2,22 +2,47 @@
 
 
 # 2):
-from itertools import *
-
-
-def f(x, y, z, w):
-    return (x and not y) or (y == z) or (not w)
-
-
-for t in product([0, 1], repeat=4):
-    table = [(t[0], t[1], 0, 0),
-             (1, 0, t[2], 0),
-             (1, 0, 1, t[3])]
-    if len(table) == len(set(table)):
-        for p in permutations('xyzw'):
-            if [f(**(dict(zip(p, r)))) for r in table] == [0, 0, 0]:
-                print(p)
+# from itertools import *
+#
+#
+# def f(x, y, z, w):
+#     return (x and not y) or (y == z) or (not w)
+#
+#
+# for t in product([0, 1], repeat=4):
+#     table = [(t[0], t[1], 0, 0),
+#              (1, 0, t[2], 0),
+#              (1, 0, 1, t[3])]
+#     if len(table) == len(set(table)):
+#         for p in permutations('xyzw'):
+#             if [f(**(dict(zip(p, r)))) for r in table] == [0, 0, 0]:
+#                 print(p)
 # # Ответ - wzyx
 
 
 # 3) 3570
+
+
+# 5)
+# s=[]
+# for N in range(1, 1000):
+#     b = bin(N)[2:]
+#     if N % 3 == 0:
+#         b = b + b[-3:]
+#     else:
+#         b = b + (bin(3 * (N % 3))[2:])
+#     R = int(b, 2)
+#     if R>151:
+#         s.append(R)
+# print(min(s))
+
+# Ответ - 163
+
+
+# 6) Ответ: 275
+
+
+# 7) Ответ: 288
+
+
+# 8)Ответ: 180
