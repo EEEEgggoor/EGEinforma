da046ee0da09236ffc2bf5b76a981aa13a293757
Student <glugach3@gmail.com>
2024-02-01 16:25:54 +0300

    123

--- nummers/24/24nummer.py
+++ nummers/24/24nummer.py
@@ -1,8 +1,3 @@
-
-
-
-
-
 # Текстовый файл состоит не более чем из 106 символов A, B и C. Определите максимальное количество идущих подряд символов C
 # f = open('zadanie24_1.txt').readline()
 # k = 1
@@ -105,14 +100,37 @@
 # print(Counter(a))
 
 
-from collections import Counter
+# from collections import Counter
+#
+# f = open('24 (3).txt').readlines()
+# a=[]
+# k=0
+# for i in range(len(f)):
+#     k+=1
+#     for x in range(len(f[i])):
+#         a1 = []
+#         a.append(f[i][x])
+#     a.append(Counter(a).most_common(-1))
 
-f = open('24 (3).txt').readlines()
-a=[]
-k=0
-for i in range(len(f)):
-    k+=1
-    for x in range(len(f[i])):
-        a1 = []
-        a.append(f[i][x])
-    a.append(Counter(a).most_common(-1))
+
+# f = open("24-4.txt").readline()
+# s=''
+# a=[]
+# for i in range(len(f)-1):
+#     if ord(f[i])<ord(f[i+1]):
+#         s+=f[i]
+#         a.append(s+f[i+1])
+#     else:
+#         s=''
+# print(max(a, key=len), len(max(a, key=len)))
+
+
+import string
+a = open("C:/Users/Student/Desktop/24-274.txt").readline()
+a = a.replace("PCCSGO", "6")
+a = a.replace("CSGO", "4")
+a = a.replace("PC", "2")
+alf = string.ascii_uppercase
+for l in alf:
+    a = a.replace(l, " ")
+print(max([sum([int(x) for x in s]) for s in a.split()]))
