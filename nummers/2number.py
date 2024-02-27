c4faeb213e77072618cbe9539ce54e8092e17093
Student <222@.a.a>
2024-02-27 16:20:37 +0300

    2 num

--- nummers/2number.py
+++ nummers/2number.py
@@ -21,14 +21,34 @@
 #                     print(x, y, z, w)
 
 
-def f(x, y, z, w):
-    return w or (x <= y) and (not z <= x)
+# def f(x, y, z, w):
+#     return w or (x <= y) and (not z <= x)
+#
+#
+# print('x y z w')
+# for x in range(0, 2):
+#     for y in range(0, 2):
+#         for z in range(0, 2):
+#             for w in range(0, 2):
+#                 if f(x, y, z, w) == 0:
+#                     print(x, y, z, w)
 
 
-print('x y z w')
-for x in range(0, 2):
-    for y in range(0, 2):
-        for z in range(0, 2):
-            for w in range(0, 2):
-                if f(x, y, z, w) == 0:
-                    print(x, y, z, w)
+# 6691
+from itertools import *
+
+
+# def f(x, y, z, w):
+#     return ((z == (not y)) and (not (x) or y) and w)
+#
+#
+# for a in product([0, 1], repeat=6):
+#     table = [(1, a[0], a[1], 0),
+#              (0, 0, a[2], 1),
+#              (a[3], a[4], a[5], 1)]
+#     if len(table)==len(set(table)):
+#         for x in permutations('xyzw'):
+#             if [f(**dict(zip(x, r))) for r in table]==[1, 1, 1]:
+#                 print(x)
+
+
