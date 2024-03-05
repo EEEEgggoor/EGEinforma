d2b5f911b8d0933dd1699a14ce0b78e44a438f73
Student <222@.a.a>
2024-03-05 15:42:52 +0300

    2

--- nummers/2number.py
+++ nummers/2number.py
@@ -33,3 +33,32 @@ from itertools import *
 #                     [f1(**dict(zip(x, r))) for r in table][2] == 0 and [f2(**dict(zip(x, r))) for r in table][2] == 1):
 #                 print(x)
 
+
+# 7664
+# def f(a, b, c, d):
+#     return ((a and b) == (not (c))) and (b <= d)
+#
+#
+# p = ['c', 'a', 'd', 'b']
+# table = [(1, 0, 0, 0),
+#          (1, 0, 1, 0),
+#          (1, 0, 1, 1),
+#          (1, 1, 0, 0)]
+# if [f(**dict(zip(p, r))) for r in table]==[1, 1, 1, 1]:
+#     print(1)
+
+
+# 2695
+def f(x, y, z, w):
+     return (w or y) and (x <= (not (z))) and (not w)
+
+sp = set()
+for a in product([0, 1], repeat=5):
+    t = [(a[0], 0, a[1], 0),
+         (1, a[2], a[3], a[4]),
+         (1, 1, 0, 0)]
+    if len(t)==len(set(t)):
+        for x in permutations('xyzw'):
+            if [f(**dict(zip(x, r))) for r in t] == [1, 1, 1]:
+                sp.add(x)
+print(len(sp))
