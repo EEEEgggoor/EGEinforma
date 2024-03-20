c472f6110e0bc1175e78507c5cdd9951174d87cf
Георгий Лыгач <glugach3@gmail.com>
2024-03-20 20:53:26 +0300

    26

--- 18.03.24_26number/26.py
+++ 18.03.24_26number/26.py
@@ -1,28 +1,32 @@
-# f = open('26-7356.txt')
-# n = f.readline()
-# f = [list(map(int, x.split())) for x in f]
-# f.sort(reverse=True)
+# f = [int(x) for x in open('26-45.txt')]
 # f = f[1:]
-# kup = 8*24
-# minikup = 8*24
-# det = 0
+# sp = []
+# f.sort()
 #
 #
-# for i in f:
-#     if i[0]>2 and (kup>0):
-#         kup+=1
-#         det+=i[1]
-#     if i[0]<=2 and (kup>0):
-#         kup-=1
-#         det+=i[1]
-# print(det)
+# def BinarySearch(lys, val):
+#     first = 0
+#     last = len(lys)-1
+#     index = -1
+#     while (first <= last) and (index == -1):
+#         mid = (first+last)//2
+#         if lys[mid] == val:
+#             index = mid
+#         else:
+#             if val<lys[mid]:
+#                 last = mid -1
+#             else:
+#                 first = mid +1
+#     if index!=-1:
+#         return True
+#     if index==-1:
+#         return False
+# mx=-11111
+# k=0
+# for i in range(len(f)):
+#     for j in range(i, len(f)-1):
+#         if (f[i]+f[j])%2==0 and BinarySearch(f, (f[i]+f[j])/2):
+#             k+=1
+#             mx = max(mx, (f[i]+f[j])/2)
+# print(k, mx)
 
-
-f = [int(x) for x in open('26-45.txt')]
-f = f[1:]
-sp = []
-for i in range(len(f)):
-    for j in range(len(f)):
-        if (f[i]+f[j])%2==0 and ((f[i]+f[j])//2 in f):
-            sp.append(f[i]+f[j])
-print(len(sp))
