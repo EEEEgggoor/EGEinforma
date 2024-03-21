9b06c2f6d5a0ae84a7a184941e35e7cdedd3eafc
Student <222@.a.a>
2024-03-21 13:14:07 +0300

    26

--- 18.03.24_26number/26.py
+++ 18.03.24_26number/26.py
@@ -1,32 +1,17 @@
-# f = [int(x) for x in open('26-45.txt')]
-# f = f[1:]
-# sp = []
-# f.sort()
-#
-#
-# def BinarySearch(lys, val):
-#     first = 0
-#     last = len(lys)-1
-#     index = -1
-#     while (first <= last) and (index == -1):
-#         mid = (first+last)//2
-#         if lys[mid] == val:
-#             index = mid
-#         else:
-#             if val<lys[mid]:
-#                 last = mid -1
-#             else:
-#                 first = mid +1
-#     if index!=-1:
-#         return True
-#     if index==-1:
-#         return False
-# mx=-11111
-# k=0
-# for i in range(len(f)):
-#     for j in range(i, len(f)-1):
-#         if (f[i]+f[j])%2==0 and BinarySearch(f, (f[i]+f[j])/2):
-#             k+=1
-#             mx = max(mx, (f[i]+f[j])/2)
-# print(k, mx)
+f = [int(x) for x in open('26_3761.txt')]
+f = f[1:]
+
+
+
+
+mx=-11111
+k=0
+for i in range(len(f)-1):
+    for j in range(i+1, len(f)):
+        sm = f[i]+f[j]
+        if (sm%2==0) and sm//2 in f:
+            k+=1
+            mx = max(mx, sm//2)
+print(k, mx)
+
 
