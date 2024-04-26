948330234e2de96ab6bac0882e037c4f48c47fa3
Student <222@.a.a>
2024-04-26 15:24:14 +0300

    2669, 2660

--- nummers/27/числа/2669/2669.py
+++ nummers/27/числа/2669/2669.py
@@ -1,9 +1,9 @@
 f = open('27-b.txt')
 n = int(f.readline())
 f = [int(x) for x in f]
-otv = []
+min1 = -100000
 for i in range(n-6):
     for j in range(i+6, n):
         if (f[j]*f[j])%2==0:
-            otv.append(f[i]*f[j])
-print(min(otv))
+            min1 = min(min1, f[i]*f[j])
+print(min1)
