9246dca0ae80bf1e3dab5c6bcd0580f180317988
Student <222@.a.a>
2024-04-26 15:24:29 +0300

    2669, 2660

--- nummers/27/числа/2669/2669.py
+++ nummers/27/числа/2669/2669.py
@@ -1,7 +1,7 @@
 f = open('27-b.txt')
 n = int(f.readline())
 f = [int(x) for x in f]
-min1 = -100000
+min1 = 111111100000
 for i in range(n-6):
     for j in range(i+6, n):
         if (f[j]*f[j])%2==0:
